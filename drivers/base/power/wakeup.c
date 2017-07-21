26850999c51d53e01bc784818b869478bb01ebed
Francisco Franco <franciscofranco.1990@gmail.com>
2017-07-22 00:56:11 +0300

    drivers: wakeup: squash wakelock blocking
    squash commit from franciscofranco:
    
    *power: wakeup: prevent IPA_WS wakelock from being acquired by default
    
    *drivers: wakeup: bypass two WiFi wakelocks
    
    *drivers: wakeup: add options to disable timerfd, netlink and wlan wakelocks
    
    As usual it's the user's entire responsibility of breaking (or not) functionality when disabling wakelocks.
    wlan_wow_wl, wlan and netmgr_wl wakelocks are disabled by default.
    
    *drivers: wakeup: more thoroughly deactivation of wakelocks
    
    For some reason deactivating the selected wakelocks wasn't being effective, some slipped through
    specially the ones that were acquired from an external module (no idea why or how).
    With this change they'll always be deactivated if they are active in the resume<->suspend
    routine, no chance to survive.
    
    *drivers: wakeup: run the wakelock blockers during wakeup_source activation and every resume
    
    Clean up the implementation to make it usable in more than one function.
    Run it every resume to make sure the wakeup source isn't still alive and kicking.
    
    *drivers: wakeup: there's no much point in running the blockers during screen on
    
    *drivers: wakeup: it's pointless to output the active wakeup sources during screen on, no need to go through the rcu locks and list iterations every now and then
    
    *drivers: wakeup: be more thorough with blocking wakelocks
    
    This patch has been suggested by dorimanx https://forum.xda-developers.com/oneplus-3t/development/kernel-franco-kernel-r1-12nd-december-t3517519/post71609905
    
    *drivers: wakeup: allow 1 wakeup from IPA_WS every now and then
    
    Apparently fully cutting out IPA_WS wakelock will introduce jitter
    during screen off BT connections.
    
    Patch suggested by dorimanx https://forum.xda-developers.com/oneplus-3t/development/kernel-franco-kernel-r1-12nd-december-t3517519/post71619213#post71619213
    
    *drivers: wakeup: allow to request and count wakelocks with screen on.
    
    *Disabling wakelocks count and activate with screen ON was mistake.
    As apps cant get there wakelock set when they operate!
    So kernel tries to suspend while app is working in background.
    such as streaming music via BT.
    
    *No need to allow IPA_WS to wake kernel.
    so far didnt found reason for it any way...
    
    Now any app can request wakelocks again as should be,
    all the blocked wakelocks cant.
    
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: ahmedradaideh <ahmed.radaideh@gmail.com>
    Signed-off-by: Subhaam <derksoul98@gmail.com>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -26,6 +26,12 @@ static bool enable_wlan_extscan_wl_ws = true;
 module_param(enable_wlan_extscan_wl_ws, bool, 0644);
 static bool enable_ipa_ws = true;
 module_param(enable_ipa_ws, bool, 0644);
+static bool enable_wlan_ws = true;
+module_param(enable_wlan_ws, bool, 0644);
+static bool enable_timerfd_ws = true;
+module_param(enable_timerfd_ws, bool, 0644);
+static bool enable_netlink_ws = true;
+module_param(enable_netlink_ws, bool, 0644);
 
 /*
  * If set, the suspend/hibernate code will abort transitions to a sleep state
@@ -456,6 +462,37 @@ static void wakeup_source_deactivate(struct wakeup_source *ws)
 		wake_up(&wakeup_count_wait_queue);
 }
 
+static bool wakeup_source_blocker(struct wakeup_source *ws)
+{
+	unsigned int wslen = 0;
+
+	if (ws) {
+		wslen = strlen(ws->name);
+
+		if ((!enable_ipa_ws && !strncmp(ws->name, "IPA_WS", wslen)) ||
+			(!enable_wlan_extscan_wl_ws &&
+				!strncmp(ws->name, "wlan_extscan_wl", wslen)) ||
+			(!enable_qcom_rx_wakelock_ws &&
+				!strncmp(ws->name, "qcom_rx_wakelock", wslen)) ||
+			(!enable_wlan_ws &&
+				!strncmp(ws->name, "wlan", wslen)) ||
+			(!enable_timerfd_ws &&
+				!strncmp(ws->name, "[timerfd]", wslen)) ||
+			(!enable_netlink_ws &&
+				!strncmp(ws->name, "NETLINK", wslen))) {
+			if (ws->active) {
+				wakeup_source_deactivate(ws);
+				pr_info("forcefully deactivate wakeup source: %s\n",
+					ws->name);
+			}
+
+			return true;
+		}
+	}
+
+	return false;
+}
+
 /*
  * The functions below use the observation that each wakeup event starts a
  * period in which the system should not be suspended.  The moment this period
@@ -531,13 +568,15 @@ static void wakeup_source_activate(struct wakeup_source *ws)
  */
 static void wakeup_source_report_event(struct wakeup_source *ws)
 {
-	ws->event_count++;
-	/* This is racy, but the counter is approximate anyway. */
-	if (events_check_enabled)
-		ws->wakeup_count++;
+	if (!wakeup_source_blocker(ws)) {
+		ws->event_count++;
+		/* This is racy, but the counter is approximate anyway. */
+		if (events_check_enabled)
+			ws->wakeup_count++;
 
-	if (!ws->active)
-		wakeup_source_activate(ws);
+		if (!ws->active)
+			wakeup_source_activate(ws);
+	}
 }
 
 /**
@@ -758,7 +797,9 @@ void pm_print_active_wakeup_sources(void)
 	list_for_each_entry_rcu(ws, &wakeup_sources, entry) {
 		if (ws->active) {
 			pr_info("active wakeup source: %s\n", ws->name);
-			active = 1;
+
+			if (!wakeup_source_blocker(ws))
+				active = 1;
 		} else if (!active &&
 			   (!last_activity_ws ||
 			    ktime_to_ns(ws->last_time) >
