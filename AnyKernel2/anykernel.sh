c9d55dbedca9e13961acaff031d5e975c09234f6
FireLord1 <firelord.xda@lineageos.org>
2017-09-14 15:23:42 +0530

    scripts: misc: Add anykernel2 support
    Change-Id: I6ec56ef0f5d43437f078b43239aa5b6ef5cd96d9
    Signed-off-by: FireLord1 <firelord.xda@gmail.com>
    Signed-off-by: FireLord1 <firelord.xda@lineageos.org>

---
+++ AnyKernel2/anykernel.sh
@@ -0,0 +1,32 @@
+# AnyKernel2 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() {
+kernel.string=FireKernel by FireLord @ xda-developers
+do.devicecheck=1
+do.modules=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=p2a42
+device.name2=p2
+device.name3=kuntao
+device.name4=kuntao_row
+device.name5=P2a42
+} # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. /tmp/anykernel/tools/ak2-core.sh;
+
+## AnyKernel install
+dump_boot;
+write_boot;
+
+## end install
--- build.sh
+++ build.sh
@@ -1,5 +1,116 @@
+#!/bin/bash
+BUILD_START=$(date +"%s")
+
+# Colours
+blue='\033[0;34m'
+cyan='\033[0;36m'
+yellow='\033[0;33m'
+red='\033[0;31m'
+nocol='\033[0m'
+
+# Kernel details
+KERNEL_NAME="FireKernel"
+VERSION="v1"
+DATE=$(date +"%d-%m-%Y-%I-%M")
+DEVICE="kuntao"
+FINAL_ZIP=$KERNEL_NAME-$VERSION-$DATE-$DEVICE.zip
+defconfig=p2a42-fk_defconfig
+
+# Dirs
+KERNEL_DIR=~/android/kernel/lenovo/msm8953
+ANYKERNEL_DIR=$KERNEL_DIR/AnyKernel2
+KERNEL_IMG=$KERNEL_DIR/arch/arm64/boot/Image.gz-dtb
+UPLOAD_DIR=~/android/kernel/upload/$DEVICE
+
+# Export
 export ARCH=arm64
 export CROSS_COMPILE=~/android/kernel/toolchain/google-64-4.9/bin/aarch64-linux-android-
-make mrproper
-make p2a42_defconfig
-make -j10
+export KBUILD_BUILD_USER="aman"
+export KBUILD_BUILD_HOST="FireLord"
+
+## Functions ##
+
+# Make kernel
+function make_kernel() {
+  echo -e "$cyan***********************************************"
+  echo -e "          Initializing defconfig          "
+  echo -e "***********************************************$nocol"
+  make $defconfig
+  echo -e "$cyan***********************************************"
+  echo -e "             Building kernel          "
+  echo -e "***********************************************$nocol"
+  make -j10
+  if ! [ -a $KERNEL_IMG ];
+  then
+    echo -e "$red Kernel Compilation failed! Fix the errors! $nocol"
+    exit 1
+  fi
+}
+
+# Making zip
+function make_zip() {
+mkdir -p tmp_mod
+make -j4 modules_install INSTALL_MOD_PATH=tmp_mod INSTALL_MOD_STRIP=1
+find tmp_mod/ -name '*.ko' -type f -exec cp '{}' $ANYKERNEL_DIR/modules/ \;
+cp $KERNEL_IMG $ANYKERNEL_DIR
+mkdir -p $UPLOAD_DIR
+cd $ANYKERNEL_DIR
+zip -r9 UPDATE-AnyKernel2.zip * -x README UPDATE-AnyKernel2.zip
+mv $ANYKERNEL_DIR/UPDATE-AnyKernel2.zip $UPLOAD_DIR/$FINAL_ZIP
+}
+
+# Options
+function options() {
+echo -e "$cyan***********************************************"
+  echo "          Compiling FireKernel kernel          "
+  echo -e "***********************************************$nocol"
+  echo -e " "
+  echo -e " Select one of the following types of build : "
+  echo -e " 1.Dirty"
+  echo -e " 2.Clean"
+  echo -n " Your choice : ? "
+  read ch
+
+  echo -e " Select if you want zip or just kernel : "
+  echo -e " 1.Get flashable zip"
+  echo -e " 2.Get kernel only"
+  echo -n " Your choice : ? "
+  read ziporkernel
+
+case $ch in
+  1) echo -e "$cyan***********************************************"
+     echo -e "          	Dirty          "
+     echo -e "***********************************************$nocol"
+     make_kernel ;;
+  2) echo -e "$cyan***********************************************"
+     echo -e "          	Clean          "
+     echo -e "***********************************************$nocol"
+     make clean
+     make mrproper
+     rm -rf tmp_mod
+     make_kernel ;;
+esac
+
+if [ "$ziporkernel" = "1" ]; then
+     echo -e "$cyan***********************************************"
+     echo -e "     Making flashable zip        "
+     echo -e "***********************************************$nocol"
+     make_zip
+else
+     echo -e "$cyan***********************************************"
+     echo -e "     Building Kernel only        "
+     echo -e "***********************************************$nocol"
+fi
+}
+
+# Clean Up
+function cleanup(){
+rm -rf $ANYKERNEL_DIR/Image.gz-dtb
+rm -rf $ANYKERNEL_DIR/modules/*.ko
+}
+
+options
+cleanup
+BUILD_END=$(date +"%s")
+DIFF=$(($BUILD_END - $BUILD_START))
+echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
