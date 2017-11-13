dfc319bbca73312de783e0222f0721876bf523e4
Subhaam <derksoul98@gmail.com>
2017-11-13 12:57:01 +0530

    anykernel2: Add magma flare
    Signed-off-by: Subhaam <derksoul98@gmail.com>

--- AnyKernel2/anykernel.sh
+++ AnyKernel2/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() {
-kernel.string=FireKernel by FireLord @ xda-developers
+kernel.string=Magma by Subham
 do.devicecheck=1
 do.modules=1
 do.cleanup=1
