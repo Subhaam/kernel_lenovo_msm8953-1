2056c28a9245fffc2de635426f569674bd0c814d
Subhaam <derksoul98@gmail.com>
2017-12-26 16:48:01 +0530

    Update to v6
    Signed-off-by: Subhaam <derksoul98@gmail.com>

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ nocol='\033[0m'
 
 # Kernel details
 KERNEL_NAME="MagmaKernel"
-VERSION="v5"
+VERSION="v6"
 DATE=$(date +"%d-%m-%Y-%I-%M")
 DEVICE="kuntao"
 FINAL_ZIP=$KERNEL_NAME-$VERSION-$DEVICE-$DATE.zip
